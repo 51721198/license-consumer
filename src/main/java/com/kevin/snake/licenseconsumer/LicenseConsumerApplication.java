5c3f8c014e96d8db139a28aaf5ba3833b0a15c55
liudun <liudun@drilver.com>
2018-03-18 22:53:07 +0800

    init

---
+++ src/main/java/com/kevin/snake/licenseconsumer/LicenseConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.kevin.snake.licenseconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseConsumerApplication.class, args);
+	}
+}
