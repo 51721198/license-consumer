5f5b094ee19b6ec415d97f93300fb7f42bb25ba9
liudun <liudun@drilver.com>
2018-03-28 16:00:17 +0800

    fix

--- src/main/java/com/kevin/snake/licenseconsumer/service/impl/HelloServiceImpl.java
+++ src/main/java/com/kevin/snake/licenseconsumer/service/impl/HelloServiceImpl.java
@@ -2,6 +2,7 @@ package com.kevin.snake.licenseconsumer.service.impl;
 
 import com.kevin.snake.licenseconsumer.service.HelloService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.exception.HystrixBadRequestException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,13 +15,14 @@ public class HelloServiceImpl implements HelloService {
     private RestTemplate restTemplate;
 
     @Override
-    @HystrixCommand(fallbackMethod = "helloFallback")
+    @HystrixCommand(fallbackMethod = "helloFallback", ignoreExceptions = {HystrixBadRequestException.class})
     public String sayHello() {
 
         return restTemplate.getForEntity("http://LICENSE/user/showAllUsers", String.class).getBody();
     }
 
-    public String helloFallback(){
+    //e为hystrix命令执行过程中抛出的异常
+    public String helloFallback(Throwable e){
         return "this is an error!!!!";
     }
 }
